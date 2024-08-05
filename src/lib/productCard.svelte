42c40694a3be5773bfa892b7f3441c409d096b02
Eric - Wall Sound <emoolenbeek@gmail.com>
2024-08-05 15:09:26 -0400

    add product card

---
+++ src/lib/productCard.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  export let product: Product = {id: "", name: "", price: 0};
+  console.log(product);
+</script>
+
+<div class="card">
+  <header class="card-header"><h2>{product.name}</h2></header>
+  <div class="card-body px-4">
+    Quantity: <strong>4</strong>
+  </div>
+  <div class="card-body px-4">
+    Price: ${product.price}
+  </div>
+  <div class="card-footer">
+    <button class="p-2 rounded variant-glass-primary">Add</button>
+    <button class="p-2 rounded variant-glass-error">Remove</button>
+  </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,35 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script lang="ts">
+	import ProductCard from "$lib/productCard.svelte";
+
+	const products : Product[] = [
+		{
+			id: "1",
+			name: "Coffee",
+			price: 5
+		},
+		{
+			id: "2",
+			name: "Banana",
+			price: 1
+		},
+		{
+			id: "3",
+			name: "Apple",
+			price: 2
+		}
+	]
+</script>
 
 <div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
-		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
-			<li><code class="code">/src/app.postcss</code> - app wide css</li>
-			<li>
-				<code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents
-			</li>
-		</ul>
+	<div class="grid grid-cols-3">
+		<div class="col-span-3">
+			<h1>Svelte Store</h1>
+		</div>
+		{#each products as product}
+		<ProductCard product={product} />
+		{/each}
+		<div class="p-2 col-span-3">
+			<button class="btn variant-filled-primary">Checkout</button>
+		</div>
 	</div>
 </div>
