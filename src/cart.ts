97ab75e9b4cfeb3e2e8c08c39806856ecdb74db8
Eric - Wall Sound <emoolenbeek@gmail.com>
2024-08-05 20:33:54 -0400

    add cart

---
+++ src/cart.ts
@@ -0,0 +1,48 @@
+import { writable, get } from "svelte/store";
+export const cartItems = writable<CartItem[]>([]);
+
+// add one to cart
+export const addToCart = (id: string) => {
+  const items = get(cartItems);
+  const itemPosition = items.findIndex(
+    (item) => { return item.id == id}
+  )
+
+  if(itemPosition !== -1) {
+    cartItems.update(() => {
+      const updatedItems = items.map( (item) => {
+        if(item.id === id) {
+          return { ...item, quantity: item.quantity + 1 };
+        }
+        return item;
+      })
+      return updatedItems;
+    })
+  } else {
+    cartItems.update(() => {
+      return [ ...items, {id: id, quantity: 1} ]
+    })
+  }
+}
+
+// remove one from cart
+export const removeFromCart = (id: string) => {
+  const items = get(cartItems);
+  const itemPosition = items.findIndex(
+    (item) => { return item.id == id}
+  )
+
+  if(items[itemPosition]?.quantity -1 === 0) {
+    items.splice(itemPosition, 1);
+  }
+
+  cartItems.update(() => {
+    const updatedItems = items.map( (item) => {
+      if(item.id === id) {
+        return { ...item, quantity: item.quantity - 1 };
+      }
+      return item;
+    })
+    return updatedItems;
+  })
+}
