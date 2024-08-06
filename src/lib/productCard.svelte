ab32f4bce799b7b19a12e513c8143dad970dde68
Eric - Wall Sound <emoolenbeek@gmail.com>
2024-08-05 20:33:17 -0400

    add cart functionality

--- src/lib/productCard.svelte
+++ src/lib/productCard.svelte
@@ -1,18 +1,30 @@
 <script lang="ts">
-  export let product: Product = {id: "", name: "", price: 0};
-  console.log(product);
+  import { get } from "svelte/store";
+  import { cartItems, addToCart, removeFromCart } from "../cart";
+  export let product : Product = {id: "", name:"", price: 0};
+  let cart = get(cartItems); 
+  let cartItemIndex = cart.findIndex((item) => { return item.id === product.id });
+  let cartProduct = cart[cartItemIndex];
+  cartItems.subscribe((newCartValue) => {
+      cart = newCartValue;
+      cartItemIndex = cart.findIndex((item) => { return item.id === product.id });
+      cartProduct = cart[cartItemIndex];
+      console.log(cart);
+  });
 </script>
 
 <div class="card">
   <header class="card-header"><h2>{product.name}</h2></header>
+  {#if cartProduct !== undefined}
+      <div class="card-body px-4">
+          Quantity: <strong>{cartProduct.quantity}</strong>
+      </div>
+  {/if}
   <div class="card-body px-4">
-    Quantity: <strong>4</strong>
+      Price: ${product.price}
   </div>
-  <div class="card-body px-4">
-    Price: ${product.price}
-  </div>
-  <div class="card-footer">
-    <button class="p-2 rounded variant-glass-success">Add</button>
-    <button class="p-2 rounded variant-glass-error">Remove</button>
-  </div>
+  <footer class="card-footer">
+      <button class="p-2 rounded variant-glass-primary" on:click={() => addToCart(product.id)}>Add</button>
+      <button class="p-2 rounded variant-glass-error"   on:click={() => removeFromCart(product.id)}>Remove</button>
+  </footer>
 </div>
